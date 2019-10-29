da9f60b245523cb6632925d7936eb97c6c834cde
vinhnv33 <v.vinhnv33@vinid.net>
2019-10-30 00:37:30 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
-import { Component } from "@angular/core";
-import * as algoliasearch from "algoliasearch/lite";
+import { Component, OnInit } from '@angular/core';
+import * as algoliasearch from 'algoliasearch/lite';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 const searchClient = algoliasearch(
-  "OXKZLGNU4D", //B1G2GM9NG0
-  "1e3e6a6d6b994bd31f3f2dc5061bf269" //aadef574be1f9252bb48d4ea09b5cfe5
+  'OXKZLGNU4D', //B1G2GM9NG0
+  '1e3e6a6d6b994bd31f3f2dc5061bf269' //aadef574be1f9252bb48d4ea09b5cfe5
 );
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   config = {
-    indexName: "merchantTagging",
+    indexName: 'merchantTagging',
     searchClient
   };
+  constructor(private fb: FormBuilder) {}
+  parentForm: FormGroup;
 
   public searchParameters = {
-    query: ""
+    query: ''
   };
 
   public setQuery({ query }: { query: string }) {
     this.searchParameters.query = query;
+  }
+  ngOnInit() {
+    this.parentForm = this.fb.group({
+      merchant_tag: [this.fb.array([])]
+    });
   }
 }
--- src/app/autocompletesss/autocomplete.component.ts
+++ src/app/autocompletesss/autocomplete.component.ts
@@ -1,21 +1,14 @@
-import {
-  Component,
-  Inject,
-  forwardRef,
-  Output,
-  EventEmitter
-} from "@angular/core";
-import { BaseWidget, NgAisInstantSearch } from "angular-instantsearch";
-import { connectAutocomplete } from "instantsearch.js/es/connectors";
-import { FormControl } from "@angular/forms";
-import {
-  MatAutocompleteSelectedEvent,
-  MatAutocomplete
-} from "@angular/material/autocomplete";
-import { COMMA, ENTER } from "@angular/cdk/keycodes";
+import { Component, Inject, forwardRef, Output, EventEmitter, ViewChild, ElementRef, Input } from '@angular/core';
+import { BaseWidget, NgAisInstantSearch } from 'angular-instantsearch';
+import { connectAutocomplete } from 'instantsearch.js/es/connectors';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 @Component({
-  selector: "app-autocomplete",
-  templateUrl: "./autocomplete.component.html"
+  selector: 'app-autocomplete',
+  templateUrl: './autocomplete.component.html',
+  styleUrls: ['autocomplete.component.css']
 })
 export class AutocompleteComponent extends BaseWidget {
   state: {
@@ -23,14 +16,24 @@ export class AutocompleteComponent extends BaseWidget {
     refine: Function;
     indices: object[];
   };
-  fruitCtrl = new FormControl();
+  merchant_tag = new FormArray([]);
+  @ViewChild('merchantInput') merchantInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
   @Output() onQuerySuggestionClick = new EventEmitter<{ query: string }>();
-
+  @Input() parentForm: FormGroup;
+  public merchantTags: any = {
+    dataSource: [],
+    visible: true,
+    selectable: true,
+    removable: true,
+    addOnBlur: true,
+    separatorKeysCodes: [ENTER, COMMA]
+  };
   constructor(
     @Inject(forwardRef(() => NgAisInstantSearch))
     public instantSearchParent
   ) {
-    super("AutocompleteComponent");
+    super('AutocompleteComponent');
   }
 
   public handleChange($event: KeyboardEvent) {
@@ -40,5 +43,25 @@ export class AutocompleteComponent extends BaseWidget {
   public ngOnInit() {
     this.createWidget(connectAutocomplete, {});
     super.ngOnInit();
+  }
+  addTag(event: MatChipInputEvent): void {
+    if (!this.matAutocomplete.isOpen) {
+      const { input, value = '' } = event;
+      if (value.trim()) {
+        this.merchant_tag.push(new FormControl(value.trim()));
+      }
+      if (input) {
+        input.value = '';
+      }
+    }
+  }
+
+  removeTag(index: number): void {
+    this.merchant_tag.removeAt(index);
+  }
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.merchantInput.nativeElement.value = '';
+    // this.onQuerySuggestionClick.emit({ query: event.option.value });
+    this.merchant_tag.push(new FormControl(event.option.value.trim()));
   }
 }
